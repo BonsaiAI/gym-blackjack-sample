c92e41ee829c6ab86b7557dc0cedd97a0111fdff
Shane Arney <shane.arney@bonsai.ai>
2016-09-26 16:31:09 -0700

    First commit of simulator and inkling files

---
+++ README.md
@@ -0,0 +1,2 @@
+# gym-blackjack-sample
+Examples of creating a simulator by integrating Bonsai's SDK with OpenAI Gym's Blackjack environment
---
+++ blackjack_simulator.py
@@ -0,0 +1,68 @@
+import gym
+import logging
+import time
+from functools import reduce
+
+import bonsai
+from bonsai_gym_common import GymSimulator
+
+ENVIRONMENT = 'Blackjack-v0'
+RECORD_PATH = None
+SKIPPED_FRAME = 1
+RECORDING_TIME = 40*60*60
+
+
+class BlackJackSimulator(GymSimulator):
+
+    def __init__(self, env, skip_frame, record_path, render_env=False):
+        super().__init__(
+            env, skip_frame=skip_frame,
+            record_path=record_path, render_env=render_env)
+
+    def get_state(self):
+        # Step 1: Perform the action and update the game along with
+        # the reward.
+        average_reward = 0
+        for i in range(self._skip_frame):
+            observation, reward, done, info = self.env.step(self._action)
+            self._frame_count += 1
+            average_reward += int(reward)
+            self.gym_total_reward += int(reward)
+
+            # Step 2: Render the game.
+            if self._render_env:
+                self.env.render()
+
+            if done:
+                break
+        self._reward = average_reward / (i + 1)
+
+        time_from_start = time.time() - self._start_time
+        if self._is_recording and (time_from_start > RECORDING_TIME):
+            self.env.monitor.close()
+
+        # Step 3: Get the current frames, and append it to deque to get current
+        # state.
+        current_frame = self.process_observation(observation)
+        self._append_state(current_frame)
+
+        # We append all of the states in the deque by adding the rows.
+        current_state = reduce(
+            lambda accum, state: accum + state, self._state_deque)
+
+        # Step 4: Check if we should reset
+        self._check_terminal(done)
+
+        # Step 6: Convert thhe observation to an inkling schema.
+        state_schema = self.get_state_schema(current_state)
+        return {"current_sum": int(state_schema[0]),
+                "dealer_card": int(state_schema[1]),
+                "usable_ace": int(state_schema[2])}
+
+if __name__ == "__main__":
+    env = gym.make(ENVIRONMENT)
+    logging.basicConfig(level=logging.INFO)
+    base_args = bonsai.parse_base_arguments()
+    simulator = BlackJackSimulator(env, SKIPPED_FRAME, RECORD_PATH)
+    bonsai.run_with_url("blackjack_simulator", simulator,
+                        base_args.brain_url)
